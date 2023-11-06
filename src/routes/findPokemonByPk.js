7e9f2198d1f6b71e8d13745048b835cb1e969f41
Franck CICCOTTI <franckciccotti@me.com>
2023-11-06 21:14:10 +0100

    sequelize - api et bdd

---
+++ src/routes/findPokemonByPk.js
@@ -0,0 +1,11 @@
+const { Pokemon } = require('../db/sequelize')
+  
+module.exports = (app) => {
+  app.get('/api/pokemons/:id', (req, res) => {
+    Pokemon.findByPk(req.params.id)
+      .then(pokemon => {
+        const message = 'Un pokémon a bien été trouvé.'
+        res.json({ message, data: pokemon })
+      })
+  })
+}
